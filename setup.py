f93004ff7ae0b403dca68979e6ac36d89025681a
Anonymous41145 <onyedikachiorji63@gmail.com>
2024-10-07 21:07:19 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# Modified by @Anonymous41145
+# Telegram Group: http://t.me/linux_repo
+# Please give me credits if you use any codes from here.
+
+import os
+import sys
+import configparser
+
+re = "\033[1;31m"
+gr = "\033[1;32m"
+cy = "\033[1;36m"
+
+def banner():
+    os.system('clear')
+    print(f"""
+    {re}╔═╗{cy}┌─┐┌┬┐┬ ┬┌─┐
+    {re}╚═╗{cy}├┤  │ │ │├─┘
+    {re}╚═╝{cy}└─┘ ┴ └─┘┴   v1.2
+    """)
+
+banner()
+
+print(gr + "[+] Installing requirements...")
+if os.system('python3 -m pip install telethon') != 0:
+    print(re + "[!] Failed to install telethon.")
+    sys.exit(1)
+
+banner()
+if not os.path.exists('config.data'):
+    cpass = configparser.RawConfigParser()
+    cpass.add_section('cred')
+    xid = input(gr + "[+] Enter API ID: " + re)
+    cpass.set('cred', 'id', xid)
+    xhash = input(gr + "[+] Enter Hash: " + re)
+    cpass.set('cred', 'hash', xhash)
+    xphone = input(gr + "[+] Enter Phone Number: " + re)
+    cpass.set('cred', 'phone', xphone)
+    
+    with open('config.data', 'w') as setup:
+        cpass.write(setup)
+
+print(gr + "[+] Setup complete!")
+print(gr + "[+] Now you can run any tool!")
+print(gr + "[+] Make sure to read README.md before using this tool.")
+print(gr + "[+] https://github.com/AbirHasan2005/TelegramScraper/blob/master/README.md")
+print("\033[92m[+] Telegram Group: \033[96mhttp://t.me/linux_repo\033[0m")
